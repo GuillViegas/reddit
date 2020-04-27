c481d2bfa7e5253db39e75aa0406f360d89f1cc1
GuilViegas <guivfaria@gmail.com>
2020-04-26 21:32:20 -0300

    Design of post and comment model

--- src/post/models.py
+++ src/post/models.py
@@ -4,17 +4,22 @@ from django.db import models
 class Post(models.Model):
     id = models.CharField(primary_key=True, max_length=6)
     title = models.CharField(max_length=300, blank=True, null=True)
-    content = models.CharField(max_length=40000, blank=True, null=True)
+    body = models.CharField(max_length=40000, blank=True, null=True)
     url = models.CharField(max_length=100, blank=True, null=True)
     subreddit = models.ForeignKey('post.SubReddit', on_delete=models.PROTECT)
-    author = models.ForeignKey('post.RedditUser', on_delete=models.PROTECT)
+    author = models.ForeignKey('post.RedditUser', on_delete=models.SET_NULL)
     score = models.PositiveIntegerField()
-
-
+    num_comments = models.PositiveIntegerField()
+    created_at = models.DateTimeField()
 
 
 class Comment(models.Model):
-    pass
+    author = models.ForeignKey('post.RedditUser', on_delete=models.PROTECT)
+    body = models.CharField(max_length=40000, blank=True, null=True)
+    score = models.PositiveIntegerField()
+    created_at = models.DateTimeField()
+    post = models.ForeignKey("post.Post", null=True, on_delete=models.PROTECT)
+    parent = models.ForeignKey("post.Comment", null=True, on_delete=models.PROTECT)
 
 
 class RedditUser(models.Model):
@@ -22,5 +27,4 @@ class RedditUser(models.Model):
 
 
 class SubReddit(models.Model):
-    author = models.ForeignKey('post.RedditUser', on_delete=models.PROTECT)
-    created_at = models.DateTimeField()
+    pass
